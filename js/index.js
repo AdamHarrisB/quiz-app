c48106819a2e565159338b2c58842dadc276a22b
Adam Harris <adamtjharris@gmail.com>
2025-03-20 19:22:31 +0100

    adding darkmode css

---
+++ js/index.js
@@ -0,0 +1,4 @@
+const bodyElement = document.querySelector('[data-js="body"]');
+
+const toggleMode = document.querySelector('[data-js="toggle-button"]');
+toggleMode.addEventListener("click", () => {bodyElement.classList.toggle("dark");});
