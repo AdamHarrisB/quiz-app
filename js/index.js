2334096c5e16972acbae63c981a0a489534c8205
Adam Harris <adamtjharris@gmail.com>
2025-03-23 17:12:04 +0100

    update html pages

--- js/index.js
+++ js/index.js
@@ -1,4 +1,17 @@
 const bodyElement = document.querySelector('[data-js="body"]');
 
+
 const toggleMode = document.querySelector('[data-js="toggle-button"]');
 toggleMode.addEventListener("click", () => {bodyElement.classList.toggle("dark");});
+
+const checkbox = document.querySelector('[data-js="bookmark"]');
+
+checkbox.addEventListener("input", (event) => {
+
+});
+
+const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
